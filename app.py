8dc2a23c99bb45ff57da342866453ea00aee864c
Sahil Singh <99865976+Sahilsingh1998@users.noreply.github.com>
2022-02-24 13:24:41 +0530

    update

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return'hello, world'
 
+@app.route('/') 
+def products():
+    return'this ia product page'  
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
