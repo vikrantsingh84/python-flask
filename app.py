93733e11c718de4928270385cf44fc1b2796b473
Sahil Singh <99865976+Sahilsingh1998@users.noreply.github.com>
2022-02-24 13:53:00 +0530

    updateedddd

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return'hello, world'
+    return render_template('index.html')
 
 @app.route('/') 
 def products():
     return'this ia product page'  
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=8000) 
