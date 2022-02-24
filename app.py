e5a0c4afdfa911f97e27d6abadcded3a6577ccb5
Sahil Singh <99865976+Sahilsingh1998@users.noreply.github.com>
2022-02-24 13:21:55 +0530

    Create a Website

---
+++ app.py
@@ -0,0 +1,10 @@
+from distutils.log import debug
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return'hello, world'
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
